4130a237c977e7bad7e003e27b827ce9da11d521
Rene Saenz <rene.saenz@gmail.com>
2017-06-04 20:09:42 -0500

    vagrant machine using CentOS

---
+++ centos72/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Update
+sudo yum -y update
+
+sudo yum -y install kernel-devel kernel-headers
+
+sudo yum -y install g++ gcc make
+#sudo yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
+
+# Update ruby
+sudo yum install ruby
+sudo yum install ruby-rdoc ruby-devel
+
+
+
+# Install docker
+#sudo yum install -y docker
+
+# Install the docker gem to interact with chef kitchen
+#gem install kitchen-docker
+
+# Install docker
+#curl -fsSL https://get.docker.com/ | sh
+#sudo service docker start
+
+
+
